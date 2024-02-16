f06ee22657445d9076535917e3782dc5504c41ce
Andi Abdul Hafez Ramadhan <etchzel@gmail.com>
2024-02-16 21:18:54 +0700

    Added parameter output

--- modules/options.py
+++ modules/options.py
@@ -5,3 +5,4 @@ class UserOptions(PipelineOptions):
   def _add_argparse_args(cls, parser):
     parser.add_value_provider_argument('--bucket_name', help='Name of the bucket to get the file from')
     parser.add_value_provider_argument('--prefix', help='Name of the folder prefix for the files to search')
+    parser.add_value_provider_argument('--output', help='Name of the folder for the output file')
